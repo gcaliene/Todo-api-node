c88b908c1c0310ca37c6867ae2701a08380ea793
Gerson Calienes <gersoncalienes@gmail.com>
2017-11-10 14:51:55 -0500

    changed server setting

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 //promises didn't used to be a part of js, so we tell mongoose to use builtin promises
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'), {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
   useMongoClient: true //this line has to be added due to deprecation
 });
 
