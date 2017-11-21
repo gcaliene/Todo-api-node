b9d166a90eed3565d9f3c46efa3fd3aedc52c5c1
Gerson Calienes <gersoncalienes@gmail.com>
2017-11-20 19:41:54 -0500

    finishing patch routes and tests

--- server/server.js
+++ server/server.js
@@ -21,10 +21,11 @@ app.use(bodyParser.json());//the return value from this json method is a functio
 
 
 //////////      ////  POST  ////  todos   //////
-app.post('/todos', (req,res) => {
+app.post('/todos', authenticate, (req,res) => { //to make the app private we have to add the authenticate middleware
   //console.log(req.body); //where the body gets stored by body-Parser
   var todo = new Todo ({
-    text: req.body.text
+    text: req.body.text,
+    _creator:req.user._id
   });
   todo.save().then((doc) => {
     res.send(doc);
@@ -96,8 +97,10 @@ app.get('/users/me', authenticate, (req,res) => {
 
 
 ////we want all the todos
-app.get('/todos' , (req,res) => {
-  Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req,res) => { //just the todos for the currently logged in user
+  Todo.find({
+    _creator: req.user._id
+  }).then((todos) => {
     res.send({todos}) //when passing back an array, create an object. it opens up to a more flexible future
   }, (e) => {
     res.status(400).send(e.errors.text.message);
@@ -106,7 +109,7 @@ app.get('/todos' , (req,res) => {
 
 ///''''''""""""""""""""""""" Get Todos by ID ''''''''''''
 
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
   console.log(req.params.id);
   var id = req.params.id;
 
@@ -114,7 +117,10 @@ app.get('/todos/:id', (req, res) => {
     return res.status(404).send();
   }
 
-  Todo.findById(id).then((todo) => {
+  Todo.findOne({
+    _id: id,
+    _creator: req.user._id
+  }).then((todo) => {
     if (!todo) {
       return res.status(404).send();
     }
@@ -129,14 +135,17 @@ app.get('/todos/:id', (req, res) => {
   // res.send(req.params); sending a get request will show you the inputteed id
 
 /////////////\\\\\\\\\\\\\\\\\\\\\ DELETE ////////////\\\\\\\\\\\\\\\\\
-app.delete('/todos/:id', (req, res) => {
+app.delete('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
-  Todo.findByIdAndRemove(id).then((todo) => {
+  Todo.findOneAndRemove({
+    _id: id,
+    _creator: req.user._id
+    }).then((todo) => {
     if (!todo) {
       return res.status(404).send();
     }
@@ -151,7 +160,7 @@ app.delete('/todos/:id', (req, res) => {
 
 //////////////\\\\\\\\\\\\ PATCH ///////////\\\\\\\\\\\\\\
 
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']); //this is the reason for ladash, the req.body is where the updaates are stored
 
@@ -166,7 +175,10 @@ app.patch('/todos/:id', (req, res) => {
     body.completedAt = null;
   }
 
-  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+  Todo.findOneAndUpdate({
+    _id: id,
+    _creator: req.user._id
+  }, {$set: body}, {new: true}).then((todo) => {
     if (!todo) {
       return res.status(404).send();
     }
